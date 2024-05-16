be0283ccf18f292e5ba1355534f83c201467629e
Ismaaeel-A <ismaaeelahmed42@gmail.com>
2024-05-16 15:48:07 +0200

    loops

---
+++ script/code.js
@@ -0,0 +1,21 @@
+let numbers = [6, 24, 67, 2, 75, 32, 70, 2]
+
+/* let even
+for (i = 0; i < numbers.length; i++) {
+    if (numbers[i] % 2 == 0) {
+        console.log(numbers[i])
+
+        console.log(`${numbers[i]} is at position ${i}`)
+    }
+} */
+
+
+/* let cnt = 0
+while (cnt < numbers.length) {
+    console.log(numbers[cnt])
+    cnt++
+} */
+
+/* for (let n in numbers){                        //n is a counter variable, can be anything. in - index
+    console.log(`Index: ${n}, Element: ${numbers[n]}`)
+} */
